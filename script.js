600b051c308c3399e740a3425362a5d68d453809
iammiguelmendes <79658617+iammiguelmendes@users.noreply.github.com>
2021-08-04 17:46:41 +0100

    Portfolio
    My commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // function to make red boarder appear
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+            $(".logocontent").removeClass("editred");
+
+        }else{
+        	$('.navbar').removeClass("sticky");
+            $('.logocontent').addClass("editred");
+
+        }
+        
+        
+    });
+    
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Tester"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+   
+     var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Tester"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+});
