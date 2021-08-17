67be7d8d38ce1fedc1396262a856507bcc3ca0b6
iammiguelmendes <79658617+iammiguelmendes@users.noreply.github.com>
2021-08-18 00:48:44 +0100

    Updated Photoshop Text

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ $(document).ready(function(){
     
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Tester"],
+        strings: ["Developer", "Tester", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
    
      var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Tester"],
+        strings: ["Developer", "Tester", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
