48e62d657fb51381cd4edf66b1c473fed5882e13
iammiguelmendes <79658617+iammiguelmendes@users.noreply.github.com>
2021-09-30 14:57:15 +0100

    Updated Website

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ $(document).ready(function(){
     
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Tester"],
+        strings: ["Designer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
    
      var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Tester"],
+        strings: ["Designer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
